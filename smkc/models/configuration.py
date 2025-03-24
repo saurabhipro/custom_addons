28123131cd24ab50eb41d022fa76301754641f86
anjli kumari <anjli.kumari@in2ittech.com>
2025-03-25 00:17:37 +0530

    remove validation smkc master

--- smkc/models/configuration.py
+++ smkc/models/configuration.py
@@ -7,24 +7,24 @@ class Zone(models.Model):
     
     name = fields.Char(string="Zone Name", required=True)
 
-    _sql_constraints = [
-        ('unique_name', 'UNIQUE(name)', 'The Zone must be unique.')
-    ]
+    # _sql_constraints = [
+    #     ('unique_name', 'UNIQUE(name)', 'The Zone must be unique.')
+    # ]
 
-    @api.constrains('name')
-    def _check_unique_zone_name(self):
-        for record in self:
-            normalized_name = record.name.strip().lower() if record.name else ''
-            print("normalized_name - ", normalized_name)
+    # @api.constrains('name')
+    # def _check_unique_zone_name(self):
+    #     for record in self:
+    #         normalized_name = record.name.strip().lower() if record.name else ''
+    #         print("normalized_name - ", normalized_name)
             
-            existing_zone = self.search([
-                ('id', '!=', record.id)
-            ])
-            for zone in existing_zone:
-                print("(existing_zone.name).lower - ",(zone.name).strip().lower())
-                if (zone.name).strip().lower() == normalized_name:
+    #         existing_zone = self.search([
+    #             ('id', '!=', record.id)
+    #         ])
+    #         for zone in existing_zone:
+    #             print("(existing_zone.name).lower - ",(zone.name).strip().lower())
+    #             if (zone.name).strip().lower() == normalized_name:
                 
-                    raise ValidationError(f"A zone with the name '{record.name}' already exists.")
+    #                 raise ValidationError(f"A zone with the name '{record.name}' already exists.")
 
 class Ward(models.Model):
     _name = 'smkc.ward'
@@ -33,18 +33,18 @@ class Ward(models.Model):
     name = fields.Char(string="Ward Name", required=True)
     zone = fields.Many2one('smkc.zone', string="Zone", required=True)
 
-    @api.constrains('name', 'zone')
-    def _check_unique_ward_name_in_zone(self):
-        for record in self:
-            existing_ward = self.search([
-                ('zone', '=', record.zone.id),
-                ('name', '=', record.name),
-                ('id', '!=', record.id)
-            ])
+    # @api.constrains('name', 'zone')
+    # def _check_unique_ward_name_in_zone(self):
+    #     for record in self:
+    #         existing_ward = self.search([
+    #             ('zone', '=', record.zone.id),
+    #             ('name', '=', record.name),
+    #             ('id', '!=', record.id)
+    #         ])
 
-            for ward in existing_ward:
-                if (ward.name).strip().lower() == (record.name).strip().lower():
-                    raise ValidationError(f"A ward with the zone '{record.zone.name}' already exists.")
+    #         for ward in existing_ward:
+    #             if (ward.name).strip().lower() == (record.name).strip().lower():
+    #                 raise ValidationError(f"A ward with the zone '{record.zone.name}' already exists.")
 
 class PropertyType(models.Model):
     _name = 'smkc.property.type'
